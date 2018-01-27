d0f7815db9c9ade728373a9286aecfa5bf83d40d
Serhii Mikheiev <mikh.s.o@gmail.com>
2018-01-27 12:27:10 +0100

    base implementation for setting targets

--- sources/features/gameplay/objectives/objectiveslogic.cpp
+++ sources/features/gameplay/objectives/objectiveslogic.cpp
@@ -4,25 +4,94 @@ ObjectivesLogic::ObjectivesLogic(MapModel* mapModel, QObject *parent)
     : QObject(parent)
     , mMapModel(mapModel)
 {
-    addObjective();
+    mSetNextObjectiveTimer.setInterval(1000);
+    mSetNextObjectiveTimer.setSingleShot(true);
+
+    connect(&mSetNextObjectiveTimer, SIGNAL(timeout()), this, SLOT(onSetNextObjectiveTimeout()));
+    mSetNextObjectiveTimer.start();
 }
 
-void ObjectivesLogic::addObjective()
+void ObjectivesLogic::setNextRandomObjective(int excludePosX, int excludePosY)
 {
-    for (int i = 0; i < mMapModel->count(); i++) {
+    QVector<TileModel*> objectiveTiles;
+    for (int i = 0; i < mMapModel->count(); i++)
+    {
         TileModel* tile = mMapModel->getTileByIndex(i);
-
-        if (tile->tileType() == TileType::Target) {
-            ObjectiveModel* objectiveModel = new ObjectiveModel(this);
-            objectiveModel->set_posX(tile->posX());
-            objectiveModel->set_posY(tile->posY());
-
-            mObjectivesList.append(objectiveModel);
-        }
+        if (tile->posX() == excludePosX && tile->posY() == excludePosY) continue;
+        if (tile->tileType() == TileType::Target) objectiveTiles.append(tile);
     }
+
+    if (objectiveTiles.length() == 0)
+    {
+        mLastDoneObjectivePosX = -1;
+        mLastDoneObjectivePosY = -1;
+        mSetNextObjectiveTimer.start();
+        return;
+    }
+
+    int randomTileIndex = qrand() % objectiveTiles.length();
+    TileModel* objectiveTile = objectiveTiles.at(randomTileIndex);
+
+    ObjectiveModel* objective = new ObjectiveModel(this);
+    objective->set_posX(objectiveTile->posX());
+    objective->set_posY(objectiveTile->posY());
+    mObjectivesList.append(objective);
+
+    connectObjective(objective);
+}
+
+void ObjectivesLogic::clearObjective(ObjectiveModel *objective)
+{
+    disconnectObjective(objective);
+    mObjectivesList.removeOne(objective);
 }
 
 QList<ObjectiveModel*> *ObjectivesLogic::objectivesList()
 {
     return &mObjectivesList;
+}
+
+void ObjectivesLogic::connectObjective(ObjectiveModel *objective)
+{
+    connect(objective, SIGNAL(isDoneChanged(bool)), this, SLOT(onObjectiveIsDone()));
+}
+
+void ObjectivesLogic::disconnectObjective(ObjectiveModel *objective)
+{
+    disconnect(objective, SIGNAL(isDoneChanged(bool)), this, SLOT(onObjectiveIsDone()));
+}
+
+void ObjectivesLogic::onSetNextObjectiveTimeout()
+{
+    setNextRandomObjective(mLastDoneObjectivePosX, mLastDoneObjectivePosY);
+}
+
+void ObjectivesLogic::onObjectiveIsDone()
+{
+    ObjectiveModel* doneObjective = nullptr;
+    for (int i = 0; i < mObjectivesList.count(); ++i)
+    {
+        ObjectiveModel* objective = mObjectivesList.at(i);
+        if (objective->isDone())
+        {
+            doneObjective = objective;
+            break;
+        }
+    }
+
+    if (doneObjective != nullptr)
+    {
+        mLastDoneObjectivePosX = doneObjective->posX();
+        mLastDoneObjectivePosY = doneObjective->posY();
+
+        clearObjective(doneObjective);
+        delete doneObjective;
+    }
+    else
+    {
+        mLastDoneObjectivePosX = -1;
+        mLastDoneObjectivePosY = -1;
+    }
+
+    mSetNextObjectiveTimer.start();
 }
--- sources/features/gameplay/objectives/objectiveslogic.h
+++ sources/features/gameplay/objectives/objectiveslogic.h
@@ -2,6 +2,7 @@
 #define OBJECTIVESLOGIC_H
 
 #include <QObject>
+#include <QTimer>
 
 #include "../map/mapmodel.h"
 #include "../map/objectivemodel.h"
@@ -13,14 +14,26 @@ class ObjectivesLogic : public QObject
 public:
     explicit ObjectivesLogic(MapModel* mapModel, QObject *parent = nullptr);
 
-    void addObjective();
-    void clearObjective();
+    void setNextRandomObjective(int excludePosX, int excludePosY);
+    void clearObjective(ObjectiveModel* objective);
 
     QList<ObjectiveModel*>* objectivesList();
 
 private:
+    void connectObjective(ObjectiveModel* objective);
+    void disconnectObjective(ObjectiveModel* objective);
+
+private slots:
+    void onSetNextObjectiveTimeout();
+    void onObjectiveIsDone();
+
+private:
     MapModel* mMapModel;
     QList<ObjectiveModel*> mObjectivesList;
+    QTimer mSetNextObjectiveTimer;
+
+    int mLastDoneObjectivePosX = -1;
+    int mLastDoneObjectivePosY = -1;
 };
 
 #endif // OBJECTIVESLOGIC_H
